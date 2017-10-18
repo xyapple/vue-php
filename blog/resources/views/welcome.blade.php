2f759a0def9dfe88e0a1c7d31a92ae24bc48e2f3
xyapple <ydeascentis@gmail.com>
2017-10-17 20:51:37 -0400

    renamed the project from ebookdb to blog

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    EBookDB
                 </div>
 
                 <div class="links">
