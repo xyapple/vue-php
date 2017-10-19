beb2b761fe1c38466f467da90cea155f5ebb5807
xyapple <ydeascentis@gmail.com>
2017-10-19 12:22:48 -0400

    display the latest() method

--- blog/app/Http/Controllers/PostsController.php
+++ blog/app/Http/Controllers/PostsController.php
@@ -10,7 +10,8 @@ class PostsController extends Controller
     //
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::orderBy('created_at', 'desc')->get();
+        
         return view('posts.index', compact('posts'));
     }
 
