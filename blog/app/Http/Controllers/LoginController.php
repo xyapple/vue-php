41e3ef9c401846cd02b24bdd649a10df2b249781
xyapple <ydeascentis@gmail.com>
2017-10-19 21:21:17 -0400

    user login form

--- blog/app/Http/Controllers/LoginController.php
+++ blog/app/Http/Controllers/LoginController.php
@@ -7,12 +7,23 @@ use App\User;
 class LoginController extends Controller
 {
     //
+
     public function create()
     {
-
-
+        return view('session.create');
     }
 
+    //save user input login
+    public function store()
+    {
+        if(!auth()->attempt(['email','password'])){
+            return back;
+        }
+        //redirect
+        return redirect('/');
+    }
+
+    //logout method
     public function destroy()
     {
         auth()->logout();
@@ -20,4 +31,5 @@ class LoginController extends Controller
         //redirect
         return redirect('/');
     }
+
 }
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -21,6 +21,8 @@ Route::get('/register', 'RegisterController@create');
 Route::post('/register', 'RegisterController@store');
 
 Route::get('/login', 'LoginController@create');
+Route::post('/login', 'LoginController@store');
+
 Route::get('/logout', 'LoginController@destroy');
 
 use App\Task;
